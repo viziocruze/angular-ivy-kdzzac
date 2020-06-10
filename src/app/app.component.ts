e1f1abde6cabef29084d412100668468f2a94974
viziocruze <seekforvinoth@outlook.com>
2020-06-10 13:46:09 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  // name = 'Angular ' + VERSION.major;
    list = [{name:'Groundnut',value:45,color:'#cba694'},{name:'Groundnut',value:55,color:'#fce384'},{name:'Groundnut',value:75,color:'#594763'},{name:'Groundnut',value:85,color:'#4e906d'},{name:'Groundnut',value:45,color:'#cba694'},{name:'Groundnut',value:55,color:'#fce384'},{name:'Groundnut',value:75,color:'#594763'},{name:'Groundnut',value:85,color:'#4e906d'}]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,7 +9,7 @@ import { DemoMaterialModule } from './material.module';
 import { ProgressBarColor } from './progress.color.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, DemoMaterialModule ],
+  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule ,DemoMaterialModule ],
   declarations: [ AppComponent, HelloComponent, ProgressBarColor ],
   bootstrap:    [ AppComponent ]
 })
